be5ab73681fc161a133975c5710e4587f82f65d7
riefarc <riefkyar.ed@protonmail.com>
2023-08-02 07:08:08 +0700

    add main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+}
+
+func checkDomain(domain string) {
+
+}
